a3fa07b736042cbb610fbaf9cfbbb9add73f69b7
Jonas sousa <jonassousagtr@gmail.com>
2023-02-08 20:34:25 -0300

    Ajustanto a logica, agora quando um item da lista esta 'checked' ele passa a 'lista de comprados', ajuste tambem do input aonde adiciona os itens, para sempre ficar em foco e ao enviar o item digitado o campo reseta para seu modo default limpando o campo de texto.

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ let listaDeItens = []
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById('itens-comprados')
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
     salvarItem()
     exibirItens()
+    itensInput.focus()
 })
 
 const salvarItem = () => {
@@ -18,28 +20,58 @@ const salvarItem = () => {
         alert('Esse item ja existe')
     }else {
     listaDeItens.push({
-        valor:comprasItem
+        valor:comprasItem,
+        checar: false
     })
     }
-
-    console.log(listaDeItens);
+    itensInput.value = ''
+    
 }
 
 const exibirItens = () => {
-    ulItens.innerHTML = ''
+        ulItens.innerHTML = ''
+        ulItensComprados.innerHTML = ''
+        
+        listaDeItens.forEach((elemento, index) => {
+            if(elemento.checar) {
+                ulItensComprados.innerHTML += `
+                    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                        <div>
+                            <input type="checkbox" checked class="is-clickable" />
+                            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-trash is-clickable deletar"></i>
+                        </div>
+                    </li>
+                `
+            }else {
+                ulItens.innerHTML += `
+                    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                        <div>
+                            <input type="checkbox" class="is-clickable" />
+                            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-trash is-clickable deletar"></i>
+                        </div>
+                    </li>    
+                    
+                    `
+            }
+        
+        })
 
-    listaDeItens.forEach((elemento, index) => {
-        ulItens.innerHTML += `
-            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-                <div>
-                    <input type="checkbox" class="is-clickable" />
-                    <input type="text" class="is-size-5" value="${elemento.valor}"></input>
-                </div>
-                <div>
-                    <i class="fa-solid fa-trash is-clickable deletar"></i>
-                </div>
-            </li>    
+        
+    let inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach( (i) => {
+        i.addEventListener('click', (evento) => {
+            let valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            exibirItens()
             
-            `
+        })
     })
+
 }
